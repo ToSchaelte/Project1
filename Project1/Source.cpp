8d45bd21683c80fc827effbd7d49cf135a8a5dd1
Tobias Schälte <tobias.schaelte@duecker.com>
2022-08-23 12:27:41 +0200

    finished tasks

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,22 +1,60 @@
 #include <iostream>
 #include <math.h>
 #include <string>
+#include <iomanip>
 using namespace std;
 
+void helloWorld();
 void test1();
+void test2();
+
+void print(string);
+void println(string);
 
 void main() {
-	test1();
+	//helloWorld();
+	//test1();
+	test2();
+}
+
+void helloWorld() {
+	println("Hello World");
 }
 
 void test1() {
 	string input;
 	int count;
-	cout << "please insert a text" << endl;
+	println("please insert a text");
 	getline(cin, input);
-	cout << "how often do you want this text to be repeated?";
+	print("how often do you want this text to be repeated?");
 	cin >> count;
 	for (int i = 0; i < count; i += 1) {
-		cout << input << endl;
+		println(input);
 	}
+}
+
+
+void test2() {
+	int number;
+
+	cout << "gib nummer: ";
+	cin >> number;
+
+	cout << "Dezimal : " << dec << number << endl;
+	cout << "Oktal : " << oct << number << endl;
+	cout << "Hexadezimal: " << hex << number << endl;
+
+	cout << setw(10) << setfill('#') << "Hallo" << endl;
+	cout << setw(10) << setfill('#') << left << "Hallo" << endl;
+	cout << setprecision(3) << 1.95583 << endl;
+}
+
+
+
+
+void print(string out) {
+	cout << out;
+}
+void println(string out) {
+	cout << out << endl;
 }
