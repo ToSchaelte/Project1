19aadec6834e1b8183c1f58d7d105286ddf4e49a
Tobias Schälte <tobias.schaelte@duecker.com>
2022-12-13 11:57:17 +0100

    enhanced task 5.7

--- Aufgabe5.7/Aufgabe5.7.cpp
+++ Aufgabe5.7/Aufgabe5.7.cpp
@@ -10,22 +10,58 @@ int main()
 	{
 		for (int b = 0; b < maxValue; ++b)
 		{
+			if (b == a)
+			{
+				continue;
+			}
 			for (int c = 0; c < maxValue; ++c)
 			{
+				if (c == a || c == b)
+				{
+					continue;
+				}
 				for (int d = 0; d < maxValue; ++d)
 				{
+					if (d == a || d == b || d == c)
+					{
+						continue;
+					}
 					for (int e = 0; e < maxValue; ++e)
 					{
+						if (e == a || e == b || e == c || e == d)
+						{
+							continue;
+						}
 						for (int f = 0; f < maxValue; ++f)
 						{
+							if (f == a || f == b || f == c || f == d || f == e)
+							{
+								continue;
+							}
 							for (int g = 0; g < maxValue; ++g)
 							{
+								if (g == a || g == b || g == c || g == d || g == e || g == f)
+								{
+									continue;
+								}
 								for (int h = 0; h < maxValue; ++h)
 								{
+									if (h == a || h == b || h == c || h == d || h == e || h == f || h == g)
+									{
+										continue;
+									}
 									for (int i = 0; i < maxValue; ++i)
 									{
+										if (i == a || i == b || i == c || i == d || i == e || i == f || i == g || i == h)
+										{
+											continue;
+										}
 										for (int j = 0; j < maxValue; ++j)
 										{
+											if (j == a || j == b || j == c || j == d || j == e || j == f || j == g || j == h || j == i)
+											{
+												continue;
+											}
 											int top_left		= a * 1000 + b * 100 + c * 10 + d;
 											int top_mid			=					   e * 10 + f;
 											int top_right		=			 e * 100 + g * 10 + d;
