4b1f8aa049be032596122954b33ce5b0ecd1e1a9
Tobias Schälte <tobias.schaelte@duecker.com>
2023-03-14 13:10:05 +0100

    cleanup

--- DatumWochentag/utils.cpp
+++ DatumWochentag/utils.cpp
@@ -22,6 +22,4 @@ bool isYearValid(int day, int month, int year)
     if (month == 2) return isLeapYear(year) ? day <= 29 : day <= 28;
     else if (month <= 7) return month % 2 == 0 ? day <= 30 : day <= 31;
     else if (month <= 12) return month % 2 == 0 ? day <= 31 : day <= 30;
-
-    return true;
 }
