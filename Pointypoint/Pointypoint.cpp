eeac0e5b45158902fa360a0b1f68145785435bca
Tobias Schälte <tobias.schaelte@duecker.com>
2023-09-08 10:55:36 +0200

    implement dynamic sorting to even and odd

--- Pointypoint/Pointypoint.cpp
+++ Pointypoint/Pointypoint.cpp
@@ -2,14 +2,44 @@
 
 using namespace std;
 
+int* generateArray(int size)
+{
+    srand(time(NULL));
+    int* numbers = new int[size];
+    for (int i = 0; i < size; ++i)
+        numbers[i] = rand() / 10;
+    return numbers;
+}
+
 int main()
 {
-    string hellu = "Hello there!";
-    string* iPointAtHellu = &hellu;
-    string& iReferenceHellu = hellu;
-    cout << *iPointAtHellu << endl
-        << iReferenceHellu << endl;
-    hellu = "General Kenobi!";
-    cout << *iPointAtHellu << endl
-        << iReferenceHellu << endl;
+    cout << "Laenge:   ";
+    int arraySize;
+    cin >> arraySize;
+    int* numbers = generateArray(arraySize);
+    cout << "Array:    ";
+    for (int i = 0; i < arraySize; ++i)
+        cout << numbers[i] << " ";
+    cout << endl;
+    int uLen = 0;
+    int gLen = 0;
+    for (int i = 0; i < arraySize; ++i)
+        if (numbers[i] % 2 == 0) ++gLen;
+        else ++uLen;
+    int* ungerade = new int[uLen];
+    int* gerade = new int[gLen];
+    int ui = 0;
+    int gi = 0;
+    for (int i = 0; i < arraySize; ++i)
+        if (numbers[i] % 2 == 0) gerade[gi++] = numbers[i];
+        else ungerade[ui++] = numbers[i];
+    cout << "Gerade:   ";
+    for (int i = 0; i < gLen; ++i)
+        cout << gerade[i] << " ";
+    cout << endl<< "Ungerade: ";
+    for (int i = 0; i < uLen; ++i)
+        cout << ungerade[i] << " ";
+    cout << endl;
+    delete[] ungerade;
+    delete[] gerade;
 }
