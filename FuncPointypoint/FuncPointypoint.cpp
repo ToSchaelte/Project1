6619ffe27a83edaf6d82deae434e2b64b39840b3
Tobias Schälte <tobias.schaelte@duecker.com>
2023-11-03 08:57:58 +0100

    enhance FuncPointypoint

--- FuncPointypoint/FuncPointypoint.cpp
+++ FuncPointypoint/FuncPointypoint.cpp
@@ -3,33 +3,22 @@
 
 using namespace std;
 
-void hexadezimal(int zahl)
-{
-	cout << hex << zahl;
-}
-
-void dezimal(int zahl)
-{
-	cout << dec << zahl;
-}
-
-void oktal(int zahl)
-{
-	cout << oct << zahl;
-}
-
 int main()
 {
 	int eingabe, zahl;
-	void (*pfunk[3]) (int) = { hexadezimal,dezimal,oktal };
-	cout << "Bitte die Zahl eingeben: ";
+	void (*pfunk[3]) (int) = {
+		[](int zahl) { cout << hex << zahl; },
+		[](int zahl) { cout << dec << zahl; },
+		[](int zahl) { cout << oct << zahl; }
+	};
+	cout << "Please insert a number: ";
 	cin >> zahl;
 	do
 	{
-		cout << endl << "Welche umwandlung? (0:end, 1:hex, 2:dez, 3:okt): ";
+		cout << endl << "Which conversion? (0:end, 1:hex, 2:dec, 3:oct): ";
 		cin >> eingabe;
 		if (eingabe > 0 && eingabe < 4) pfunk[eingabe - 1](zahl);
-		else if (eingabe != 0) cout << endl << "Ungueltige Eingabe" << endl;
+		else if (eingabe != 0) cout << endl << "invalid input" << endl;
 		else cout << endl << "bye bye \\(^_^)/" << endl;
 	} while (eingabe);
 	return 0;
