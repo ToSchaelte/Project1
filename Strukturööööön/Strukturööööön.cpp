0093e6cdaa314fd971da22eb185eab9edab580af
Tobias Schälte <tobias.schaelte@outlook.de>
2024-01-12 09:41:39 +0100

    refactor Strukturööööön.cpp

--- Strukturööööön/Strukturööööön.cpp
+++ Strukturööööön/Strukturööööön.cpp
@@ -2,40 +2,40 @@
 
 using namespace std;
 
-struct Student
+struct student
 {
-	int id = 1000+(rand() % 9000);
-	string lastName;
-	string firstName;
-	int pcNumber;
+    int id = 1000 + rand() % 9000;
+    string last_name;
+    string first_name;
+    int pc_number;
 };
 
 int main()
 {
-	srand(time(NULL));
-	auto studentCount = 20;
-	auto students = new Student[studentCount];
-	for (auto i = 0; i < studentCount; ++i)
-	{
-		int pcNr;
-		cout << "PC Number: ";
-		cin >> pcNr;
-		if (pcNr == 0)
-		{
-			studentCount = i;
-			break;
-		}
-		students[i] = Student();
-		students[i].pcNumber = pcNr;
-		cout << "First name: ";
-		cin >> students[i].firstName;
-		cout << "Last name: ";
-		cin >> students[i].lastName;
-		cout << endl;
-	}
-	cout << endl;
-	for (auto i = 0; i < studentCount; ++i)
-		cout << "The ID of the student " << students[i].firstName << " " << students[i].lastName
-		<< " at the PC " << students[i].pcNumber << " is: " << students[i].id << endl;
-	delete[] students;
+    srand(time(nullptr));
+    auto student_count = 20;
+    const auto students = new student[student_count];
+    for (auto i = 0; i < student_count; ++i)
+    {
+        int pc_nr;
+        cout << "PC Number: ";
+        cin >> pc_nr;
+        if (pc_nr == 0)
+        {
+            student_count = i;
+            break;
+        }
+        students[i] = student();
+        students[i].pc_number = pc_nr;
+        cout << "First name: ";
+        cin >> students[i].first_name;
+        cout << "Last name: ";
+        cin >> students[i].last_name;
+        cout << '\n';
+    }
+    cout << '\n';
+    for (auto i = 0; i < student_count; ++i)
+        cout << "The ID of the student " << students[i].first_name << " " << students[i].last_name
+            << " at the PC " << students[i].pc_number << " is: " << students[i].id << '\n';
+    delete[] students;
 }
