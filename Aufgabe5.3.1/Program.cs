37b925aed25d0af2d7c603d4e2220f9809b37c01
Tobias Schälte <tobias.schaelte@duecker.com>
2024-03-22 11:19:51 +0100

    Add C#-Tasks 5.3.1 & 5.3.2

---
+++ Aufgabe5.3.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_5_3_1
+{
+    internal static class Program
+    {
+        private const int PasswordValue = 'a' + 'u' + 't' + 'o';
+
+        private static void Main()
+        {
+            var input = new char[4];
+            for (var j = 0; j < 3; j++)
+            {
+                for (var i = 0; i < input.Length; i++)
+                {
+                    input[i] = Convert.ToChar(Console.Read());
+                    if (input[i] == '\n' || input[i] == '\r') i--;
+                }
+
+                if (!IsPasswordCorrect(input))
+                {
+                    Console.WriteLine($"!!! WRONG PASSWORD. {3 - 1 - j} ATTEMPTS LEFT !!!.");
+                    if (j >= 2) Console.WriteLine("!!! YOU FAILED !!!");
+                    continue;
+                }
+
+                Console.WriteLine("LOGIN CORRECT");
+                break;
+            }
+        }
+
+        private static bool IsPasswordCorrect(IEnumerable<char> chars)
+        {
+            return chars.Select(char.ToLower).Aggregate(0, (val, c) => val + c) == PasswordValue;
+        }
+    }
+}
---
+++ Aufgabe5.3.2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_5_3_2
+{
+    internal abstract class Program
+    {
+        public static void Main()
+        {
+            var input = new char[5];
+            var errorMessage = string.Empty;
+            var isValid = true;
+            do
+            {
+                if (!isValid) Console.WriteLine(errorMessage);
+                Console.Write("Please enter the Code: ");
+                for (var i = 0; i < input.Length; i++)
+                {
+                    input[i] = Convert.ToChar(Console.Read());
+                    if (input[i] == '\n' || input[i] == '\r') i--;
+                }
+            } while (!(isValid = IsCodeValid(input, out errorMessage)));
+
+            Console.WriteLine("INPUT CORRECT");
+        }
+
+        private static bool IsCodeValid(IReadOnlyList<char> wannabeCode, out string errorMessage)
+        {
+            errorMessage = "!!! INVALID CODE !!!";
+            var inputAsNumber = ToNumber(wannabeCode);
+            if (inputAsNumber % 3 == 0 || inputAsNumber % 5 == 0 || inputAsNumber % 7 == 0) return false;
+            if ('1' == wannabeCode[0] && '1' != wannabeCode[4]) return false;
+            if (wannabeCode.Aggregate('0' - wannabeCode[wannabeCode.Count - 1], (sum, c) => sum + c - '0') % 7 !=
+                wannabeCode[4] - '0') return false;
+            errorMessage = string.Empty;
+            return true;
+        }
+
+        private static int ToNumber(IReadOnlyList<char> chars)
+        {
+            var num = 0;
+            for (var i = 0; i < chars.Count; i++)
+            {
+                if (!char.IsDigit(chars[i])) throw new ArgumentException("char is not a number");
+                num += (chars[i] - '0') * (int)Math.Pow(10, i);
+            }
+
+            return num;
+        }
+    }
+}
